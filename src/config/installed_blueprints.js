eeac04a8cc153edfa8b980004afcdd1e412a418b
Crowdbotics <team@crowdbotics.com>
2020-06-10 02:13:18 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings64802', human_name: 'Settings', access_route: 'Settings64802'},
 { name: 'Settings64794', human_name: 'Settings', access_route: 'Settings64794'},
 { name: 'UserProfile64792', human_name: 'User Profile', access_route: 'UserProfile64792'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings64802Navigator from '../features/Settings64802/navigator';
 import Settings64794Navigator from '../features/Settings64794/navigator';
 import UserProfile64792Navigator from '../features/UserProfile64792/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
@@ -23,6 +24,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Settings64802: { screen: Settings64802Navigator },
 Settings64794: { screen: Settings64794Navigator },
 UserProfile64792: { screen: UserProfile64792Navigator },
 Messenger: { screen: MessengerNavigator },
